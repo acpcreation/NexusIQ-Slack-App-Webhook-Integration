69185da001df1d1386d8e9dbd6afefbff7110388
acpcreation <acpcreation@gmail.com>
2023-02-22 11:20:51 -0500

    updates

--- index.js
+++ index.js
@@ -14,29 +14,32 @@ app.use(express.json());
 
 /*****************/
 // ENVIRONMENT VARIABLES
-SLACK_URL = process.env.SLACK_URL  //"https://hooks.slack.com/services/..."
-PORT = process.env.PORT            //3000
-IQ_URL= process.env.IQ_URL         //"http://localhost:8070/"
+SLACK_URL = process.env.SLACK_URL // "https://hooks.slack.com/services/..."
+PORT = process.env.PORT // 3000
+IQ_URL = process.env.IQ_URL
+// "http://localhost:8070/"
 /*****************/
 
+
+// formatViolationAlertSlackNotification(1)
 
 
 /*****************/
 // RECEIVER
 /*****************/
-app.post('/slack', function(req, res){
+app.post('/slack', function (req, res) {
     var data = req.body;
     // console.log("New Slack Message from IQ!");
     // console.log(data)
 
-    res.send({status:200})
+    res.send({status: 200})
     processIqData(data)
 });
 
 
 // Visit url in terminal /test to trigger test meesgae (ex. localhost:3000/test )
-app.get('/test', function(req, res){ 
-    res.send({status:200, message:"Success!"})
+app.get('/test', function (req, res) {
+    res.send({status: 200, message: "Success!"})
     let payload = {
         "timestamp": "2020-04-22T18:30:04.673+0000",
         "initiator": "admin",
@@ -61,35 +64,39 @@ app.get('/test', function(req, res){
         }
     }
     processIqData(payload)
- });
+});
 
 
 // Do this for different webhook messages
 function processIqData(e) {
     // console.log(JSON.stringify(e))
     // console.log("-------------------------------")
-    
-    //Policy Management
-    if(e.hasOwnProperty("type") && e.type == "POLICY"){
+
+    // Policy Management
+    if (e.hasOwnProperty("type") && e.type == "POLICY") {
         formatPolicyActionSlackNotification(e)
     }
 
-    //Application Evaluation (detailed app data)
-    if(e.hasOwnProperty("applicationEvaluation")){
-        formatAppEvaluationSlackNotification(e)
+    // Application Evaluation (minimal data from app evaluation)
+    // if(e.hasOwnProperty("applicationEvaluation")){
+    //     formatAppEvaluationSlackNotification(e)
+    // }
+
+    // Violation alert (detailed app data)
+    if (e.hasOwnProperty("policyAlerts")) {
+        formatViolationAlertSlackNotification(e)
     }
 
-    //License Override Management
-    if(e.hasOwnProperty("licenseOverride")){
+    // License Override Management
+    if (e.hasOwnProperty("licenseOverride")) {
         formatLicenseManagementSlack(e)
     }
 
-    //TBD: Violation Alert (minimal data from app evaluation)
-    //TBD: Security Vulnerability Override Management
+    // TBD: Security Vulnerability Override Management
 }
 
 function formatAppEvaluationSlackNotification(e) {
-    let scanURL = IQ_URL+"assets/index.html#/applicationReport/"+e.applicationEvaluation.application.publicId+"/"+e.applicationEvaluation.reportId+"/policy"
+    let scanURL = IQ_URL + "assets/index.html#/applicationReport/" + e.applicationEvaluation.application.publicId + "/" + e.applicationEvaluation.reportId + "/policy"
     console.log(scanURL)
 
     let slackMsg = {
@@ -99,7 +106,7 @@ function formatAppEvaluationSlackNotification(e) {
                 "type": "header",
                 "text": {
                     "type": "plain_text",
-                    "text": "Nexus IQ Evaluation for "+ e.applicationEvaluation.application.name,
+                    "text": "Nexus IQ Evaluation for " + e.applicationEvaluation.application.name,
                     // "emoji": true
                 }
             }, {
@@ -107,14 +114,7 @@ function formatAppEvaluationSlackNotification(e) {
                 "fields": [
                     {
                         "type": "mrkdwn",
-                        "text": "*Application Evaluation Report*\n\n"+
-                                "\t*- Affected Components:*\t"+e.applicationEvaluation.affectedComponentCount+"\n"+
-                                "\t*- Critical Components:*\t"+e.applicationEvaluation.criticalComponentCount+"\n"+
-                                "\t*- Severe Components:*\t"+e.applicationEvaluation.severeComponentCount+"\n"+
-                                "\t*- Moderate Components:*\t"+e.applicationEvaluation.moderateComponentCount+"\n"+
-                                "\n\n*Evaluation Date*: \n\t"+e.applicationEvaluation.evaluationDate+"\n"+
-                                "*Stage:* "+e.applicationEvaluation.stage+"\n"+
-                                "*Outcome:* "+e.applicationEvaluation.outcome+"\n"
+                        "text": "*Application Evaluation Report*\n\n" + "\t*- Affected Components:*\t" + e.applicationEvaluation.affectedComponentCount + "\n" + "\t*- Critical Components:*\t" + e.applicationEvaluation.criticalComponentCount + "\n" + "\t*- Severe Components:*\t" + e.applicationEvaluation.severeComponentCount + "\n" + "\t*- Moderate Components:*\t" + e.applicationEvaluation.moderateComponentCount + "\n" + "\n\n*Evaluation Date*: \n\t" + e.applicationEvaluation.evaluationDate + "\n" + "*Stage:* " + e.applicationEvaluation.stage + "\n" + "*Outcome:* " + e.applicationEvaluation.outcome + "\n"
                     },
                 ]
             }, {
@@ -136,12 +136,119 @@ function formatAppEvaluationSlackNotification(e) {
     sendSlackMessage(slackMsg)
 }
 
-function formatPolicyActionSlackNotification(e){
+function formatViolationAlertSlackNotification(e) {
+    let scanURL = IQ_URL + "assets/index.html#/applicationReport/" + e.application.publicId + "/" + e.applicationEvaluation.reportId + "/policy"
+    console.log(scanURL)
+
+    // Reformat message
+    let violationJSON = []
+    e.policyAlerts = e.policyAlerts.reverse();
+    for(let i in e.policyAlerts) {
+        // console.log(e.policyAlerts[i])
+        let p = e.policyAlerts[i]
+        let violation = p.policyName+" ("+p.threatLevel+")"
+
+        if (p.threatLevel >=9){
+            violation = ":bangbang: "+violation
+        }
+        if (p.threatLevel <9 && p.threatLevel >=7){
+            violation = ":warning: "+violation
+        }
+        
+        for(let j in p.componentFacts){
+            let pj = p.componentFacts[j]
+            let displayName = pj.displayName.replaceAll(' ', '')
+
+            let description = ""
+            // for(let k in pj.constraintFacts){
+            //     for(let l in pj.constraintFacts[k].satisfiedConditions){
+            //         description+= ""+violation+" : "+pj.constraintFacts[k].satisfiedConditions[l].reason+"\n"
+            //     }
+            // }
+
+            found = false
+            for(let m in violationJSON){
+                if (displayName == violationJSON[m].name){
+                    // violationJSON[m].details += description
+                    found = true
+                    break;
+                }
+            }
+
+            if (found==false){
+                violationJSON.push({
+                    name:displayName,
+                    details:violation
+                })
+            }
+
+        }
+    }
+    
+    let violationDetails = ""
+    for(let i in violationJSON){
+        violationDetails+="\n• "+violationJSON[i].details+" | "+violationJSON[i].name
+    }
+
+    let slackMsg = {
+        "channel": "iq",
+        "blocks": [
+            {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": "New Security Violation(s) found for "+ e.application.name+" with Nexus IQ Continuous Monitoring",
+                    // "emoji": true
+                }
+            }, {
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Application Evaluation Report*\n"+
+                                "\t*• Affected Components:*\t"+e.applicationEvaluation.affectedComponentCount+"\n"+
+                                "\t*• Critical Components:*\t"+e.applicationEvaluation.criticalComponentCount+" \n"+
+                                "\t*• Severe Components:  *\t"+e.applicationEvaluation.severeComponentCount+" \n"+
+                                "\t*• Moderate Components:*\t"+e.applicationEvaluation.moderateComponentCount+"\n"+
+                                "*Stage:* "+e.applicationEvaluation.stage+"\n"+
+                                "*Outcome:* "+e.applicationEvaluation.outcome+"\n"
+                    },
+                ]
+            }, 
+            {
+                "type": "context",
+                "elements": [
+                    {
+                        "type": "plain_text",
+                        "text": violationDetails,
+                        "emoji": true
+                    }
+                ]
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "View Report"
+                        },
+                        "style": "primary",
+                        "url": scanURL
+                    }
+                ]
+            }
+        ]
+    }
+    sendSlackMessage(slackMsg)
+}
+
+function formatPolicyActionSlackNotification(e) {
     policyName = ""
-    for(let i in e.owner){
-        for(let j in e.owner[i]){
-            if(e.id == e.owner[i][j].id){
-                // console.log(e.owner[i][j].name)
+    for (let i in e.owner) {
+        for (let j in e.owner[i]) {
+            if (e.id == e.owner[i][j].id) { // console.log(e.owner[i][j].name)
                 policyName = e.owner[i][j].name;
                 break;
             }
@@ -156,14 +263,14 @@ function formatPolicyActionSlackNotification(e){
                 "type": "header",
                 "text": {
                     "type": "plain_text",
-                    "text": "Nexus IQ Administrative Action",
+                    "text": "Nexus IQ Administrative Action"
                 }
             }, {
                 "type": "section",
                 "fields": [
                     {
                         "type": "mrkdwn",
-                        "text": e.type +" "+e.action+" for *"+policyName+"* in "+e.owner.name+" "+e.owner.type.toLowerCase()+"."
+                        "text": e.type + " " + e.action + " for *" + policyName + "* in " + e.owner.name + " " + e.owner.type.toLowerCase() + "."
                     },
                 ]
             }, {
@@ -185,20 +292,20 @@ function formatPolicyActionSlackNotification(e){
     sendSlackMessage(slackMsg)
 }
 
-function formatLicenseManagementSlack(e){
+function formatLicenseManagementSlack(e) {
 
     let comments = ""
-    if(e.licenseOverride.comment.length > 1){
-        comments = "\n\nComments: \""+e.licenseOverride.comment+"\""
+    if (e.licenseOverride.comment.length > 1) {
+        comments = "\n\nComments: \"" + e.licenseOverride.comment + "\""
     }
 
     let lics = e.licenseOverride.licenseIds.toString()
     lics = lics.replaceAll(",", ", ")
 
 
-    let mainText =  "component "+ extractComponentName(e.licenseOverride.componentIdentifier)
-    if(e.licenseOverride.licenseIds.length >0){
-        mainText +=" - license(s): "+lics
+    let mainText = "component " + extractComponentName(e.licenseOverride.componentIdentifier)
+    if (e.licenseOverride.licenseIds.length > 0) {
+        mainText += " - license(s): " + lics
     }
 
     let slackMsg = {
@@ -208,14 +315,14 @@ function formatLicenseManagementSlack(e){
                 "type": "header",
                 "text": {
                     "type": "plain_text",
-                    "text": "Nexus IQ License Override",
+                    "text": "Nexus IQ License Override"
                 }
             }, {
                 "type": "section",
                 "fields": [
                     {
                         "type": "mrkdwn",
-                        "text": "License "+e.licenseOverride.status+" for "+mainText+". "+comments
+                        "text": "License " + e.licenseOverride.status + " for " + mainText + ". " + comments
                     },
                 ]
             }, {
@@ -238,15 +345,14 @@ function formatLicenseManagementSlack(e){
     sendSlackMessage(slackMsg)
 }
 
-function extractComponentName(e){
-    //Pass in componentIdentifier
-    let name  = "";
-    if(e.format == "maven"){
-        name = e.coordinates.groupId+" : "+e.coordinates.artifactId+" : "+e.coordinates.version
-        name += " ("+e.format+")"
+function extractComponentName(e) { // Pass in componentIdentifier
+    let name = "";
+    if (e.format == "maven") {
+        name = e.coordinates.groupId + " : " + e.coordinates.artifactId + " : " + e.coordinates.version
+        name += " (" + e.format + ")"
 
-    }else{
-        name = e.coordinates.packageId+" : "+e.coordinates.version+ " ("+e.format+")"
+    } else {
+        name = e.coordinates.packageId + " : " + e.coordinates.version + " (" + e.format + ")"
     }
     return name;
 }
@@ -255,30 +361,27 @@ function extractComponentName(e){
 /*****************/
 // Sender
 /*****************/
-function sendSlackMessage(e){
+function sendSlackMessage(e) {
     var url = SLACK_URL
     var sendData = JSON.stringify(e);
 
     var config = {
         method: 'post',
         url: url,
-        headers: { 
+        headers: {
             'Content-Type': 'application/json'
         },
-        data : sendData
+        data: sendData
     };
 
-    //Send new slack message
+    // Send new slack message
     axios(config).then(function (response) {
-        console.log("Slack response: "+JSON.stringify(response.data));
-    })
-    .catch(function (error) {
+        console.log("Slack response: " + JSON.stringify(response.data));
+    }).catch(function (error) {
         console.log(error);
     });
 }
 
 
-
 app.listen(PORT || 3000);
-console.log("Running on http://localhost:"+PORT+"/")
-
+console.log("Running on http://localhost:" + PORT + "/")
